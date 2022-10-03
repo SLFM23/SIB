3f0cfbcdcd3194d6fa1e64ce6931ce50a4fad0da
SLFM23 <slfm99@gmail.com>
2022-10-03 09:18:11 +0100

    Add new files

---
+++ src/si/data/dataset.py
@@ -0,0 +1,97 @@
+
+from typing import Tuple, List
+import numpy as np
+import pandas as pd
+
+class Dataset:
+    
+    def __init__(self, X:np.ndarray = None, y:np.ndarray = None, features:List = None, label:str = None):
+        """Dataset implementation from scratch"""
+        
+        self.X = X
+        self.y = y
+        self.features = features
+        self.label = label
+        
+    def get_shape(self) -> Tuple:
+        """Return dataset dimensions"""
+        
+        return self.X.shape
+    
+    def has_label(self):
+        """Returns True if dataset has labels"""
+        
+        if self.y is not None:
+            return True
+        else:
+            return False
+    
+    def get_classes(self) -> List[int]:
+        """Return the label classes as a list"""
+        
+        if self.y is None:
+            raise Exception("You've an unsupervised dataset")
+        else:
+            return np.unique(self.y)
+        
+    def get_mean(self):
+        """Return mean of features"""
+        return np.mean(self.X, axis=0)
+    
+    def get_variance(self):
+        """Return variance of features"""
+        return np.var(self.X, axis=0)
+    
+    def get_median(self):
+        """Return median of features"""
+        return np.median(self.X, axis=0)
+    
+    def get_min(self):
+        """Return the minimum value of features"""
+        return np.min(self.X, axis=0)
+    
+    def get_max(self):
+        """Return the maximum value of features"""
+        return np.max(self.X, axis=0)
+    
+    def summary(self):
+        """Return summarized statistics about the dataset."""
+        return pd.DataFrame(
+            {"mean": self.get_mean(),
+             "median": self.get_median(),
+             "variance": self.get_variance(),
+             "min": self.get_min(),
+             "max": self.get_max()}
+        )
+        
+    def dropna (self):
+        """Class method that removes samples with atleast one null (NaN) value."""
+        
+        return pd.DataFrame(self.X).dropna(axis=0).reset_index(drop=True)
+    
+    def fillna(self, value: int):
+        """Class method that fills all NaN values with the given value
+        Args:
+            value (int): Given value to replace null values with
+        """
+        
+        return pd.DataFrame(self.X).fillna(value)
+    
+    def print_dataframe(self):
+        """Prints dataframe in pandas DataFrame format
+        """
+        return pd.DataFrame(self.X, columns=self.features, index=self.y)
+    
+    
+if __name__ == "__main__":
+    x = np.array([[1, 2, 8], [1, 2, 7]])
+    y = np.array([1, 2])
+    features = ["A", "B", "C"]
+    label = "y"
+    a = Dataset(X=x, y=y, features=features, label=label)
+    b = Dataset()
+    c = Dataset(X=x, y=y, features=features, label=None)
+    #print(a.get_shape())
+    print(c.has_label())
+    #print(a.get_classes())
+    print(c.print_dataframe())
---
+++ src/si/io/csv_file.py
@@ -0,0 +1,70 @@
+import sys
+sys.path.insert(0, '/Users/Sergiomendes/Desktop/SIB/SIB/src/si')
+
+from typing import Optional, Union
+import pandas as pd
+import numpy as np
+
+from data.dataset import Dataset
+
+def read_csv(filename:str, sep:str = ",", features: Optional[bool] = True, label: Union[None, int]= None):
+    """Function that reads csv file and returns a Dataset object of that file.
+    Args:
+        filename (str): name/path of file
+        sep (str): separator between values. Defaults to , .
+        features (Optional[bool], optional): If the csv file has feature names. Defaults to True.
+        label (int): If the dataset has defined labels receives an integer value that tells the column of the labels. Defaults to None.
+    """
+    imported_data = pd.read_csv(filepath_or_buffer=filename, sep=sep)
+    data = imported_data.values.tolist()
+    headers = list(imported_data.columns)
+    header_label = headers[label]
+    
+    if features:
+        if label is not None:
+            del headers[label]
+    else:
+        headers = None
+    
+    if label is not None:
+        y = list(imported_data.iloc[:, label])
+        imported_data = imported_data.drop(imported_data.columns[label], axis=1)
+        data = imported_data.values.tolist()
+    else: y = None
+    
+    return Dataset(X=data, y=y, features = headers, label = header_label)
+        
+    
+def write_csv(dataset, filename: str, sep: str = ",", features: Optional[bool] = True, label: Optional[bool] = True):
+    """Writes a csv file from a dataset object
+    Args:
+        dataset (_type_): Dataset to save on csv format
+        filename (str): Name of the csv file that will be saved
+        sep (str, optional): Separator of values. Defaults to ",".
+        features (Optional[bool], optional): Boolean value that tells if the dataset object has feature names. Defaults to True.
+        label (Optional[bool], optional): Boolean value that tells if the dataset object has label names Defaults to True.
+    """
+    csv = pd.DataFrame(data=dataset.X)
+    
+    if features:
+        csv.columns = dataset.features
+    
+    if label:
+        csv.insert(loc=0, column=dataset.label, value=dataset.y)
+        
+    csv.to_csv(filename, sep = sep, index=False)
+    
+    
+if __name__ == "__main__":
+    # TESTING
+    # file = r"/home/rui/Desktop/SIB/si/datasets/iris.csv"
+    # a = read_csv(filename=file, sep = ",", features=True, label=4)
+    # print(a.print_dataframe())
+    # print(a.summary())
+    # write_csv(a, "csv_write_test1.csv", features=True, label=False)
+    
+    # TESTING MISSING VALUES METHODS ON DATASET CLASS
+    file = r"/home/rui/Desktop/SIB/si/datasets/iris_missing_data.csv"
+    a = read_csv(filename=file, sep = ",", features=True, label=4)
+    # print(a.dropna())
+    print(a.fillna(100))
